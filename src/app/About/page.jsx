8ffb5b759c5af8d063d600541181cec0c9f93718
Rehan <rehanalire52@gmail.com>
2024-01-24 19:11:41 +0530

    Updating Responsiveness

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -10,53 +10,53 @@ export const metadata = {
 function page() {
 
   return (
-    <div className=' h-[575vh] montserrat-font w-auto'>
+    <div className=' h-[780vh] montserrat-font w-auto'>
          <div className='about-img flex justify-center items-center h-[75vh] w-auto'>
-         <h1 className='text-5xl mt-6 text-white montserrat-font tracking-widest font-bold'>ABOUT US</h1>
+         <h1 className='text-3xl mt-6 text-white montserrat-font tracking-widest font-bold'>ABOUT US</h1>
          </div>
-         <div className='w-auto h-[75vh] flex justify-center items-center flex-col'>
+         <div className='w-auto h-[90vh] flex justify-center items-center flex-col'>
          <h2 className='courgette-font text-3xl text-center text-red-600'>Italian Restaurant</h2>
                <h1 className='montserrat-font mt-4 text-5xl tracking-widest text-center font-extrabold'>OUR STORY</h1>
-               <p className=' text-center mt-10 font-semibold opacity-65'>Fusce at risus eget mi auctor pulvinar. Suspendisse maximus venenatis pretium. Orci varius natoque penatibus et <br /> magnis dis parturient montes, nascetur ridiculus mus. Aliquam purus purus, lacinia a scelerisque in, luctus vel felis. <br /> Donec odio diam, dignissim a efficitur at, efficitur et est. Pellentesque semper est ut pulvinar ullamcorper. Class <br /> aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla et leo accumsan, <br /> egestas velit ac, fringilla tortor. Sed varius justo sed luctus mattis.</p>
+               <p className=' text-center text-sm mt-10 font-semibold opacity-65'>Fusce at risus eget mi auctor pulvinar. <br /> Suspendisse maximus venenatis pretium. <br /> Orci varius natoque penatibus et  magnis dis <br /> parturient montes, nascetur ridiculus mus. <br /> Aliquam purus purus, lacinia a scelerisque in, <br /> luctus vel felis. Donec odio diam, dignissim a <br /> efficitur at, efficitur et est. Pellentesque <br /> semper est ut pulvinar ullamcorper. Class <br /> aptent taciti sociosqu ad litora torquent per <br /> conubia nostra, per inceptos himenaeos. <br /> Nulla et leo accumsan, egestas velit ac, <br /> fringilla tortor. Sed varius justo sed luctus <br /> mattis.</p>
          </div>
-         <div className='h-[80vh] flex justify-center items-center flex-col w-auto about-img-1'>
+         <div className='h-[70vh] flex justify-center items-center flex-col w-auto about-img-1'>
          <h2 className='courgette-font text-3xl text-center text-red-600'>Discover</h2>
-               <h1 className='montserrat-font mt-4 text-white text-5xl tracking-widest text-center font-extrabold'>OUR VIDEO</h1>
+               <h1 className='montserrat-font mt-4 text-white text-5xl tracking-widest text-center font-extrabold'>OUR <br /> VIDEO</h1>
          </div>
-         <div className='h-[80vh] flex justify-center gap-36 items-center w-auto  '>
-          <div className=''>
+         <div className='h-[120vh] flex flex-col mt-28 justify-center items-center w-auto  '>
+          <div className='flex justify-center mt-5 items-center flex-col'>
                <h2 className='courgette-font text-3xl text-center text-red-600'>Delicious</h2>
                <h1 className='montserrat-font mt-4 text-5xl tracking-widest text-center font-extrabold'>RECIPES</h1>
-               <p className=' text-center mt-10 font-semibold opacity-65'>Donec quis lorem nulla. Nunc eu odio mi. Morbi nec lobortis est. Sed <br /> fringilla, nunc sed imperdiet lacinia, nisl ante egestas mi, ac facilisis <br /> ligula sem id neque.</p>
+               <p className=' text-center mt-10 mb-5 text-sm font-semibold opacity-65'>Donec quis lorem nulla. Nunc eu odio mi .<br /> Morbi nec lobortis est. Sed  fringilla, nunc  sed <br /> imperdiet lacinia, nisl ante egestas mi, ac  <br /> facilisis ligula sem id neque.</p>
           </div>
-          <div className='w-[25vw] rounded-xl h-[50vh] overflow-hidden'>
-          <div className='w-[25vw] rounded-xl hover:scale-110 duration-500 h-[50vh]'>
+          <div className='w-[90vw] rounded-xl mt-16 h-[80vh] overflow-hidden'>
+          <div className='w-[90vw] rounded-xl hover:scale-110 duration-500 h-[80vh]'>
              <img src="https://preview.colorlib.com/theme/pato/images/our-story-01.jpg" alt="" />
           </div>
           </div>
         </div>
-         <div className='h-[80vh] flex justify-center gap-36 items-center w-auto  '>
-          <div className='w-[25vw] rounded-xl h-[50vh] overflow-hidden'>
-         <div className='w-[25vw] rounded-xl hover:scale-110 duration-500 h-[50vh]'>
+         <div className='h-[95vh] mb-32 flex justify-center flex-col items-center w-auto  '>
+          <div className='w-[90vw] rounded-xl h-[85vh] overflow-hidden'>
+         <div className='w-[90vw] rounded-xl hover:scale-110 duration-500 h-[85vh]'>
              <img src="https://preview.colorlib.com/theme/pato/images/our-story-02.jpg" alt="" />
           </div>
           </div>
           <div className=''>
                <h2 className='courgette-font text-3xl text-center text-red-600'>Romantic</h2>
-               <h1 className='montserrat-font mt-4 text-5xl tracking-widest text-center font-extrabold'>RESTAURANT</h1>
-               <p className=' text-center mt-10 font-semibold opacity-65'>Donec quis lorem nulla. Nunc eu odio mi. Morbi nec lobortis est. Sed <br /> fringilla, nunc sed imperdiet lacinia, nisl ante egestas mi, ac facilisis <br /> ligula sem id neque.</p>
+               <h1 className='montserrat-font mt-4 text-4xl tracking-widest text-center font-extrabold'>RESTAURANT</h1>
+               <p className=' text-center text-sm mt-10 font-semibold opacity-65'>Donec quis lorem nulla. Nunc eu odio mi .<br /> Morbi nec lobortis est. Sed  fringilla, nunc  sed <br /> imperdiet lacinia, nisl ante egestas mi, ac  <br /> facilisis ligula sem id neque.</p>
           </div>
         </div>
         <div className='h-[75vh] w-auto about-img-2'>
 
         </div>
-        <div className='h-[85vh] flex justify-center items-center flex-col w-auto'>
+        <div className='h-[170vh] flex justify-center items-center flex-col w-auto'>
                <div className='mt-8'>
                <h2 className='courgette-font text-3xl text-center text-red-600'>Meet Our</h2>
                <h1 className='montserrat-font mt-4 text-5xl tracking-widest text-center font-extrabold'>CHEF</h1>
                </div>
-               <div className='flex justify-center gap-5 mt-12 items-center'>
-                   <div className='h-[40vh] w-[25vw] flex  flex-col justify-center items-center rounded-lg border-2 border-[#d9d9d9] '>
+               <div className='flex flex-col justify-center gap-7 mt-12 items-center'>
+                   <div className='h-[40vh] w-[90vw] flex  flex-col justify-center items-center rounded-lg border-2 border-[#d9d9d9] '>
                     <div className='h-24 w-24 overflow-hidden rounded-full'>
                        <div className='chef-img h-24 w-24 border-4 border-red-600 hover:scale-105 duration-500 rounded-full bg-purple-600'>
 
@@ -66,7 +66,7 @@ function page() {
                           <h4 className='text-xs font-semibold mt-2'>CHEF</h4>
                           <p className='text-center opacity-70 mt-2'>Donec porta eleifend mauris ut effici- <br />tur. Quisque non velit vestibulum, lob- <br />ortis mi eget, rhoncus nunc</p>
                    </div>
-                   <div className='h-[40vh] flex flex-col justify-center items-center w-[25vw] rounded-lg border-2 border-[#d9d9d9]'>
+                   <div className='h-[40vh] flex flex-col justify-center items-center w-[90vw] rounded-lg border-2 border-[#d9d9d9]'>
                    <div className='h-24 w-24 overflow-hidden rounded-full'>
                        <div className='chef-img1 h-24 w-24 border-4 border-red-600 hover:scale-105 duration-500 rounded-full bg-purple-600'>
 
@@ -76,7 +76,7 @@ function page() {
                           <h4 className='text-xs font-semibold mt-2'>CHEF</h4>
                           <p className='text-center opacity-70 mt-2'>Donec porta eleifend mauris ut effici- <br />tur. Quisque non velit vestibulum, lob- <br />ortis mi eget, rhoncus nunc</p>
                    </div>
-                   <div className='h-[40vh] w-[25vw] flex flex-col justify-center items-center rounded-lg border-2 border-[#d9d9d9]'>
+                   <div className='h-[40vh] w-[90vw] flex flex-col justify-center items-center rounded-lg border-2 border-[#d9d9d9]'>
                    <div className='h-24 w-24 overflow-hidden rounded-full'>
                        <div className='chef-img h-24 w-24 border-4 border-red-600 hover:scale-105 duration-500 rounded-full bg-purple-600'>
 
@@ -88,11 +88,15 @@ function page() {
                    </div>
                </div>
         </div>
-        <div className='bg-gray-100 flex justify-center items-center gap-10 montserrat-font w-auto h-[25vh]'>
-                <h1 className='text-2xl tracking-widest font-semibold '>SPECIALS SIGN UP</h1>
-                <input type="text" className='pr-12 border outline-none border-[#d9d9d9] rounded-lg  pl-4 text-sm py-3' placeholder='Email Address' id="" /><IoMdMail className='inline-block absolute left-[900px]' />
-                <button className='bg-black text-white px-8 py-3 rounded-lg text-sm'>SINGH - UP</button>
-           </div>
+        <div className='bg-gray-100 flex flex-col justify-center items-center gap-10 montserrat-font w-auto h-[45vh] relative'>
+    <h1 className='text-xl tracking-widest font-semibold '>SPECIALS SIGN UP</h1>
+    <div className='relative'>
+        <input type="text" className='pr-16 border outline-none border-[#d9d9d9] rounded-lg pl-4 text-sm py-3' placeholder='Email Address' id="" />
+        <IoMdMail className='absolute top-1/2 transform -translate-y-1/2 right-4 text-gray-500' />
+    </div>
+    <button className='bg-black text-white px-8 py-3 rounded-lg text-sm'>SIGN UP</button>
+</div>
+
     </div>
   )
 }
--- src/app/Contact/page.jsx
+++ src/app/Contact/page.jsx
@@ -10,63 +10,63 @@ export const metadata = {
 
 function page() {
   return (
-    <div className='h-[260vh] montserrat-font w-auto'>
-         <div className='h-[75vh]  flex justify-center items-center w-auto contact-img'>
-         <h1 className='text-5xl mt-6 text-white montserrat-font tracking-widest font-bold'>CONTACT</h1>
+    <div className='min-h-screen montserrat-font w-auto'>
+         <div className='h-[75vh] flex justify-center items-center w-auto contact-img'>
+         <h1 className='text-4xl mt-6 text-white montserrat-font tracking-widest font-bold'>CONTACT</h1>
          </div>
-         <div className='flex  justify-center items-center mt-20'>
+         <div className='flex justify-center items-center mt-16'>
          <iframe
           title="Google Maps"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12370371.792462902!2d2.124175805707276!3d40.80356989527924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12d4fe82448dd203%3A0xe22cf55c24635e6f!2sItaly!5e0!3m2!1sen!2sin!4v1705746424441!5m2!1sen!2sin"
-          width="1200px"
+          width="350px"
           height="400px"
-          style={{ border: 0 }}
+          style={{ border: 2 }}
           allowFullScreen=""
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
          </div>
-         <div className='flex justify-center h-[120vh] bg-gray-100 flex-col  items-center'>
+         <divv className='flex justify-center h-[230vh] w-auto bg-gray-100 flex-col items-center'>
               <div>
                    <h1 className='text-xl tracking-widest font-semibold'>SEND US A MESSAGE</h1>
               </div>
-              <div className='flex mt-16 gap-10 justify-center items-center'>
+              <div className='flex mt-10 flex-col gap-10 justify-center items-center'>
                     <div>
                     <p className='text-sm mb-2 opacity-70'>Name</p>
-                      <input type="text" className='py-3 pr-10 outline-none pl-3 border-2 border-[#d9d9d9] rounded-lg' placeholder='Name' />
+                      <input type="text" className='py-3 pr-28 outline-none pl-3 border-2 border-[#d9d9d9] rounded-lg' placeholder='Name' />
                     </div>
                      <div>
                      <p className='text-sm mb-2 opacity-70'>Phone</p>
-                      <input type="text" className='py-3 outline-none pr-10 pl-3 border-2 border-[#d9d9d9] rounded-lg' placeholder='Phone' />
+                      <input type="text" className='py-3 outline-none pr-28 pl-3 border-2 border-[#d9d9d9] rounded-lg' placeholder='Phone' />
                      </div>
                     <div>
                     <p className='text-sm mb-2 opacity-70'>Email</p>
-                      <input type="text" className='py-3 outline-none pr-10 pl-3 border-2 border-[#d9d9d9] rounded-lg' placeholder='Email' />
+                      <input type="text" className='py-3 outline-none pr-28 pl-3 border-2 border-[#d9d9d9] rounded-lg' placeholder='Email' />
                     </div>
                   </div>
                   <div className='mt-10'>
-                  <p className='text-sm mb-2  opacity-70'>Message</p>
-                    <textarea  className='border-2 p-3 outline-none rounded-lg border-[#d9d9d9] ' placeholder='Message' name="" id="" cols="90" rows="8"></textarea>
+                  <p className='text-sm mb-2 opacity-70'>Message</p>
+                    <textarea  className='border-2 p-3 outline-none rounded-lg border-[#d9d9d9] ' placeholder='Message' name="" id="" cols="33" rows="8"></textarea>
                     </div>
                     <div className='flex justify-center items-center mt-4'>
-                  <button className='bg-black text-white px-8 py-3 text-xs montserrat-font rounded-xl'>SUBMIT</button>
+                  <button className='bg-black text-white mt-4 px-8 py-3 text-xs montserrat-font rounded-xl'>SUBMIT</button>
                   </div>
-                  <div className='flex items-center gap-60 mt-32 mb-8'>
+                  <div className='flex flex-col items-start justify-center gap-20 mt-32 mb-8'>
                       <div>
-                         <h1 className='text-xl font-semibold tracking-widest'><IoLocationSharp className='inline-block mb-1 mr-3'/>LOCATION</h1>
+                         <h1 className='text-2xl font-semibold tracking-widest'><IoLocationSharp className='inline-block mb-1 mr-3'/>LOCATION</h1>
                          <p className='mt-4 ml-8 text-sm'>8th floor, 379 Hudson St, <br /> New York, NY 10018</p>
                       </div>
                       <div>
-                         <h1 className='text-xl  mb-4 font-semibold tracking-widest'><IoMdCall className='inline-block mb-1 mr-3'/>CALL US</h1>
+                         <h1 className='text-2xl  mb-4 font-semibold tracking-widest'><IoMdCall className='inline-block mb-1 mr-3'/>CALL US</h1>
                          <p className='text-sm ml-8'>+123-456-7890</p>
                          <p className='text-sm mt-1 ml-8'>+9329003456</p>
                       </div>
                       <div>
-                         <h1 className='text-xl font-semibold tracking-widest'><LuClock10 className='inline-block mb-1 mr-3'/>OPENING HOURS</h1>
-                         <p className='mt-4 text-sm ml-8 '>09:30 AM – 11:00 PM <br /> Every Day</p>
+                         <h1 className='text-2xl font-semibold tracking-widest'><LuClock10 className='inline-block mb-1 mr-3'/>OPENING HOURS</h1>
+                         <p className='mt-4 text-sm ml-8 '>09:30 AM – 11:00 PM <br /><span className=''>Every Day</span></p>
                       </div>
                   </div>
-         </div>
+         </divv>
     </div>
   )
 }
