111ce214650c5f810aa3dcbf5c30e9c8ef4baff0
Rehan <rehanalire52@gmail.com>
2024-01-24 21:28:23 +0530

    Update

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -10,24 +10,28 @@ export const metadata = {
 function page() {
 
   return (
-    <div className=' h-[775vh] lg:h-[600vh] montserrat-font w-auto'>
+    <div className=' h-[775vh] lg:h-[565vh] montserrat-font w-auto'>
          <div className='about-img flex justify-center items-center h-[75vh] w-auto'>
          <h1 className='text-3xl lg:text-5xl mt-6 text-white montserrat-font tracking-widest font-bold'>ABOUT US</h1>
          </div>
-         <div className='w-auto h-[90vh] flex justify-center items-center flex-col'>
+         <div className='w-auto h-[90vh] lg:h-[65vh] flex  lg:pt-28 justify-center items-center flex-col'>
          <h2 className='courgette-font text-3xl text-center text-red-600'>Italian Restaurant</h2>
                <h1 className='montserrat-font mt-4 text-5xl tracking-widest text-center font-extrabold'>OUR STORY</h1>
-               <p className=' text-center text-sm mt-10 font-semibold opacity-65'>Fusce at risus eget mi auctor pulvinar. <br /> Suspendisse maximus venenatis pretium. <br /> Orci varius natoque penatibus et  magnis dis <br /> parturient montes, nascetur ridiculus mus. <br /> Aliquam purus purus, lacinia a scelerisque in, <br /> luctus vel felis. Donec odio diam, dignissim a <br /> efficitur at, efficitur et est. Pellentesque <br /> semper est ut pulvinar ullamcorper. Class <br /> aptent taciti sociosqu ad litora torquent per <br /> conubia nostra, per inceptos himenaeos. <br /> Nulla et leo accumsan, egestas velit ac, <br /> fringilla tortor. Sed varius justo sed luctus <br /> mattis.</p>
+               <div className='lg:h-[40vh] lg:w-[55vw]'>
+               <p className='lawra text-center text-sm mt-10 px-4 font-semibold opacity-65'>Fusce at risus eget mi auctor pulvinar. Suspendisse maximus venenatis pretium. Orci varius natoque penatibus et  magnis dis parturient montes, nascetur ridiculus mus. Aliquam purus purus, lacinia a scelerisque in, luctus vel felis. Donec odio diam, dignissim a efficitur at, efficitur et est. Pellentesque semper est ut pulvinar ullamcorper. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla et leo accumsan, egestas velit ac, fringilla tortor. Sed varius justo sed luctus mattis.</p>
+               </div>
          </div>
          <div className='h-[70vh] flex justify-center items-center flex-col w-auto about-img-1'>
          <h2 className='courgette-font text-3xl text-center text-red-600'>Discover</h2>
-               <h1 className='montserrat-font mt-4 text-white text-5xl tracking-widest text-center font-extrabold'>OUR <br /> VIDEO</h1>
+               <h1 className='montserrat-font mt-4 text-white lg:text-5xl text-4xl tracking-widest text-center font-extrabold'>OUR VIDEO</h1>
          </div>
-         <div className='h-[120vh] lg:h-[80vh] flex flex-col lg:gap-56 lg:mt-0 lg:flex-row mt-28 justify-center items-center w-auto  '>
-          <div className='flex justify-center mt-5 items-center flex-col'>
+         <div className='h-[120vh] lg:h-[75vh] flex flex-col lg:gap-44 lg:mt-0 lg:flex-row mt-28 justify-center items-center w-auto  '>
+          <div className='flex justify-center mt-5 lg:mt-28 items-center flex-col'>
                <h2 className='courgette-font text-3xl text-center text-red-600'>Delicious</h2>
                <h1 className='montserrat-font mt-4 text-5xl tracking-widest text-center font-extrabold'>RECIPES</h1>
-               <p className=' text-center mt-10 mb-5 text-sm font-semibold opacity-65'>Donec quis lorem nulla. Nunc eu odio mi .<br /> Morbi nec lobortis est. Sed  fringilla, nunc  sed <br /> imperdiet lacinia, nisl ante egestas mi, ac  <br /> facilisis ligula sem id neque.</p>
+               <div className='lg:h-[30vh] lg:w-[70vh]'>
+               <p className=' text-center lawra mt-10 mb-5 text-sm px-6 font-semibold lg:opacity-50 opacity-50'>Donec quis lorem nulla. Nunc eu odio mi  Morbi nec lobortis est. Sed  fringilla, nunc  sed imperdiet lacinia, nisl ante egestas mi, ac  facilisis ligula sem id neque.</p>
+               </div>
           </div>
           <div className='w-[90vw] rounded-xl lg:w-[25vw]  lg:h-[50vh] mt-16 h-[80vh] overflow-hidden'>
           <div className='w-[90vw] lg:w-[25vw] lg:h-[55vh]  rounded-xl hover:scale-110 duration-500 h-[80vh]'>
@@ -35,16 +39,18 @@ function page() {
           </div>
           </div>
         </div>
-         <div className='h-[95vh] lg:h-[80vh] mb-32 lg:mb-0 flex justify-center lg:gap-56 lg:flex-row flex-col items-center w-auto  '>
+         <div className='h-[95vh] lg:h-[75vh] mb-32 lg:mb-0 flex justify-center lg:gap-44 lg:flex-row flex-col items-center w-auto  '>
           <div className='w-[90vw] lg:w-[25vw] lg:h-[50vh] rounded-xl h-[85vh] overflow-hidden'>
          <div className='w-[90vw] lg:w-[25vw] lg:h-[55vh]  rounded-xl hover:scale-110 duration-500 h-[85vh]'>
              <img src="https://preview.colorlib.com/theme/pato/images/our-story-02.jpg" alt="" />
           </div>
           </div>
-          <div className=''>
+          <div className='lg:flex lg:justify-center mt-20 lg:items-center lg:mt-20 lg:flex-col'>
                <h2 className='courgette-font text-3xl  text-center text-red-600'>Romantic</h2>
                <h1 className='montserrat-font mt-4 text-4xl lg:text-5xl tracking-widest text-center font-extrabold'>RESTAURANT</h1>
-               <p className=' text-center text-sm mt-10 font-semibold opacity-65'>Donec quis lorem nulla. Nunc eu odio mi .<br /> Morbi nec lobortis est. Sed  fringilla, nunc  sed <br /> imperdiet lacinia, nisl ante egestas mi, ac  <br /> facilisis ligula sem id neque.</p>
+               <div className='lg:h-[30vh] lg:w-[70vh]'>
+               <p className='text-center lawra mt-10 mb-5 text-sm px-5 font-semibold lg:opacity-50 opacity-50'>Fusce iaculis, quam quis volutpat cursus, tellus quam varius eros, in euismod lorem nisl in ante. Maecenas imperdiet vulputate dui sit amet vestibulum. Nulla quis suscipit nisl.</p>
+               </div>
           </div>
         </div>
         <div className='h-[75vh] w-auto about-img-2'>
