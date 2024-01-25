1d4de5ce0fd4537b4593004a3fb5330b0f2b753a
Rehan <rehanalire52@gmail.com>
2024-01-25 23:53:18 +0530

    NAvbar update

--- src/app/Navbar/page.jsx
+++ src/app/Navbar/page.jsx
@@ -6,11 +6,12 @@ import { SiSwiggy } from "react-icons/si";
 
 function page() {
   return (
-    <nav className='w-screen montserrat-font h-[12vh] px-20 fixed text-white flex justify-around items-center'>
+    <nav className='w-screen montserrat-font h-[12vh] px-5 fixed text-white flex justify-around items-center'>
         <div className='flex justify-center items-center'>
             <img src="https://preview.colorlib.com/theme/pato/images/icons/logo.png" alt="" />
         </div>
-        <ul className='flex text-sm gap-10'>
+          <div>
+          <ul className='flex hidden text-sm gap-10'>
           <Link href="/">
             <li className='hover:text-red-600 duration-500 cursor-pointer'>HOME</li>
             </Link>
@@ -31,11 +32,18 @@ function page() {
             </Link>
 
         </ul>
-        <div className='flex items-center gap-6'>
-           <span><FaFacebookF  className='text-xl hover:text-red-600 duration-500 cursor-pointer text-white' /></span>
-           <span><FaInstagram className='text-xl hover:text-red-600 duration-500 cursor-pointer text-white'  /></span>
-           <span><SiSwiggy  className='text-xl hover:text-red-600 duration-500 cursor-pointer text-white'  /></span>
+          </div>
+       <div className='flex items-center gap-2'>
+       <div className='flex items-center gap-3'>
+           <span><FaFacebookF  className='text-lg hover:text-red-600 duration-500 cursor-pointer text-white' /></span>
+           <span><FaInstagram className='text-lg hover:text-red-600 duration-500 cursor-pointer text-white'  /></span>
+           <span><SiSwiggy  className='text-lg hover:text-red-600 duration-500 cursor-pointer text-white'  /></span>
         </div>
+        <div className='md:hidden'>
+        <button className='text-white'>Menu
+        </button>
+      </div>
+       </div>
     </nav>
   )
 }
